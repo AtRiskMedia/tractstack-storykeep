0532f50bed52e4c58a609cfe2539a890e14e4c1e
Adon <a@atriskmedia.com>
2024-08-22 15:58:38 -0400

    fixed combobox

--- src/components/storykeep/fields/ViewportComboBox.tsx
+++ src/components/storykeep/fields/ViewportComboBox.tsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import React, { useRef } from "react";
 import { Combobox } from "@headlessui/react";
 import { ChevronUpDownIcon, CheckIcon } from "@heroicons/react/20/solid";
 import {
@@ -35,13 +35,19 @@ const ViewportComboBox = ({
         : ComputerDesktopIcon;
 
   const handleChange = (newValue: string) => {
-    onChange(newValue);
+    onChange(newValue || "");
+  };
+
+  const handleSelect = (selectedValue: string) => {
+    onChange(selectedValue);
+    onFinalChange(selectedValue, viewport);
     inputRef.current?.blur();
   };
 
   const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
     if (event.key === "Enter") {
       event.preventDefault();
+      onFinalChange(value, viewport);
       inputRef.current?.blur();
     }
   };
@@ -53,14 +59,14 @@ const ViewportComboBox = ({
     >
       <Icon className="h-8 w-8 mr-2" aria-hidden="true" />
       <div className="relative w-full">
-        <Combobox value={value} onChange={handleChange}>
+        <Combobox value={value} onChange={handleSelect}>
           <div className="relative">
             <Combobox.Input
               ref={inputRef}
               className="w-full border border-gray-300 rounded-md py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-1 focus:ring-myorange focus:border-myorange"
-              onChange={event => onChange(event.target.value)}
+              onChange={event => handleChange(event.target.value)}
               onKeyDown={handleKeyDown}
-              onBlur={() => onFinalChange(value, viewport)}
+              displayValue={v => (typeof v === `string` ? v : "")}
             />
             <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
               <ChevronUpDownIcon
@@ -71,7 +77,9 @@ const ViewportComboBox = ({
           </div>
           <Combobox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
             {values
-              .filter(item => item.toLowerCase().includes(value.toLowerCase()))
+              .filter(item =>
+                item.toLowerCase().includes((value || "").toLowerCase())
+              )
               .map(item => (
                 <Combobox.Option
                   key={item}

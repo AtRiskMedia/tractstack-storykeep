802999fc00b905ec8634ec4261a9ba8bd41cebf9
Adon <a@atriskmedia.com>
2024-09-04 18:39:46 -0400

    added fix on z-index

--- src/components/storykeep/EditModal.tsx
+++ src/components/storykeep/EditModal.tsx
@@ -82,7 +82,7 @@ export const EditModal = ({ type, contentMap, id }: EditModalProps) => {
           </button>
         </div>
       </div>
-      <div className="z-[9002]">
+      <div className="relative z-[9002]">
         {$editMode?.type === `storyfragment` &&
         $editMode?.mode === `settings` ? (
           <StoryFragmentSettings id={$editMode.id} />

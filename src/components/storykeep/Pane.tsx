dc5df4809de33bec1447cb7d28e2671019ea2f58
Adon <a@atriskmedia.com>
2024-09-28 23:07:02 -0400

    fixed display on context page

--- src/components/storykeep/Pane.tsx
+++ src/components/storykeep/Pane.tsx
@@ -171,11 +171,10 @@ const Pane = (props: {
       if (!fragment?.type) return null;
       switch (fragment.type) {
         case "markdown":
-          if ($paneSlug[id].current === `introCentered`)
-            console.log(
-              $paneSlug[id].current,
-              JSON.stringify(fragment.payload.optionsPayload.classNamesPayload)
-            );
+          //console.log(
+          //  $paneSlug[id].current,
+          //  JSON.stringify(fragment.payload.optionsPayload.classNamesPayload)
+          //);
           return (
             <div
               key={`markdown-${index}`}

972018fda9abdd58f7d8b45c77d05035a57654b7
Adon <a@atriskmedia.com>
2024-10-12 01:28:07 -0400

    ok here we go! x2

--- src/components/storykeep/EditModal.tsx
+++ src/components/storykeep/EditModal.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef,useCallback } from "react";
 import { useStore } from "@nanostores/react";
 import { XMarkIcon } from "@heroicons/react/24/outline";
 import { ulid } from "ulid";

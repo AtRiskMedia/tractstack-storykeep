078428ab5633bb00212aa17e1be137010916cedb
Adon <a@atriskmedia.com>
2024-10-30 01:26:44 -0400

    add failover on no turso

--- src/api/turso.ts
+++ src/api/turso.ts
@@ -25,6 +25,15 @@ import type {
   TursoQuery,
 } from "../types.ts";
 
+function hasTurso() {
+  if (
+    !import.meta.env.TURSO_DATABASE_URL ||
+    !import.meta.env.TURSO_AUTH_TOKEN
+  ) {
+    throw new Error("Turso credentials not configured");
+  }
+}
+
 export const turso = createClient({
   url: import.meta.env.TURSO_DATABASE_URL,
   authToken: import.meta.env.TURSO_AUTH_TOKEN,
@@ -32,6 +41,7 @@ export const turso = createClient({
 
 export async function getUniqueTailwindClasses(id: string) {
   try {
+    hasTurso();
     const { rows } = await turso.execute({
       sql: `SELECT id, options_payload FROM pane WHERE id != ?`,
       args: [id],
@@ -45,6 +55,7 @@ export async function getUniqueTailwindClasses(id: string) {
 
 export async function getAllResources(): Promise<ResourceDatum[]> {
   try {
+    hasTurso();
     const { rows } = await turso.execute(`
       SELECT * FROM resource
       ORDER BY title ASC
@@ -58,6 +69,7 @@ export async function getAllResources(): Promise<ResourceDatum[]> {
 
 export async function getAllMenus(): Promise<MenuDatum[]> {
   try {
+    hasTurso();
     const { rows } = await turso.execute(`
       SELECT id, title, theme, options_payload
       FROM menu
@@ -72,6 +84,7 @@ export async function getAllMenus(): Promise<MenuDatum[]> {
 
 export async function getMenuById(id: string): Promise<MenuDatum | null> {
   try {
+    hasTurso();
     const { rows } = await turso.execute({
       sql: `SELECT id, title, theme, options_payload
             FROM menu
@@ -88,6 +101,7 @@ export async function getMenuById(id: string): Promise<MenuDatum | null> {
 
 export async function getDatumPayload(): Promise<DatumPayload> {
   try {
+    hasTurso();
     const { rows } = await turso.execute(
       `WITH dummy AS (SELECT 1),
 resource_data AS (
@@ -167,6 +181,7 @@ export async function getResourcesBySlug(
 ): Promise<ResourceDatum[]> {
   const placeholders = slugs.map(() => "?").join(",");
   try {
+    hasTurso();
     const { rows } = await turso.execute({
       sql: `SELECT * FROM resource WHERE slug IN (${placeholders})`,
       args: slugs,
@@ -184,6 +199,7 @@ export async function getResourcesByCategorySlug(
 ): Promise<ResourceDatum[]> {
   const placeholders = slugs.map(() => "?").join(",");
   try {
+    hasTurso();
     const { rows } = await turso.execute({
       sql: `SELECT * FROM resource WHERE category_slug IN (${placeholders})`,
       args: slugs,
@@ -198,6 +214,7 @@ export async function getResourcesByCategorySlug(
 
 export async function getAllTractStack(): Promise<TractStackDatum[]> {
   try {
+    hasTurso();
     const { rows } = await turso.execute(`
 SELECT id, title, slug, social_image_path FROM tractstack
     `);
@@ -212,6 +229,7 @@ export async function getTractStackBySlug(
   slug: string
 ): Promise<TractStackDatum[] | null> {
   try {
+    hasTurso();
     const { rows } = await turso.execute({
       sql: `SELECT id, title, slug, social_image_path FROM tractstack WHERE slug = ?`,
       args: [slug],
@@ -227,6 +245,7 @@ export async function getTractStackIdBySlug(
   slug: string
 ): Promise<string | null> {
   try {
+    hasTurso();
     const { rows } = await turso.execute({
       sql: `SELECT id FROM tractstack WHERE slug = ?`,
       args: [slug],
@@ -247,6 +266,7 @@ export async function getStoryFragmentBySlug(
   slug: string
 ): Promise<StoryFragmentDatum | null> {
   try {
+    hasTurso();
     const { rows } = await turso.execute({
       sql: `SELECT 
                      sf.id AS id,
@@ -333,6 +353,7 @@ export async function getContextPaneBySlug(
   slug: string
 ): Promise<ContextPaneDatum | null> {
   try {
+    hasTurso();
     const { rows } = await turso.execute({
       sql: `SELECT 
                    p.id, 
@@ -393,6 +414,7 @@ export async function getContextPaneBySlug(
 
 export async function getContentMap(): Promise<ContentMap[]> {
   try {
+    hasTurso();
     const { rows: storyfragments } = await turso.execute(
       `SELECT 
               sf.id AS id,
@@ -425,6 +447,7 @@ export async function getContentMap(): Promise<ContentMap[]> {
 
 export async function getPaneDesigns(): Promise<PaneDesign[]> {
   try {
+    hasTurso();
     const { rows } = await turso.execute(`
      WITH file_data AS (
        SELECT 
@@ -488,6 +511,7 @@ export async function getPaneDesigns(): Promise<PaneDesign[]> {
 
 export async function getFileById(id: string): Promise<FileDatum | null> {
   try {
+    hasTurso();
     const { rows } = await turso.execute({
       sql: `SELECT id, filename, alt_description, url, src_set
             FROM file
@@ -529,6 +553,7 @@ export async function getFileById(id: string): Promise<FileDatum | null> {
 
 export async function getAllFileDatum(): Promise<TursoFileNode[]> {
   try {
+    hasTurso();
     const { rows } = await turso.execute(`
       SELECT id, filename, alt_description, url, src_set
       FROM file
@@ -542,6 +567,7 @@ export async function getAllFileDatum(): Promise<TursoFileNode[]> {
 
 export async function getFullContentMap(): Promise<FullContentMap[]> {
   try {
+    hasTurso();
     const { rows } = await turso.execute(`
       SELECT id, id as slug, title, 'Menu' as type, theme, NULL as is_context_pane, NULL as category_slug
       FROM menu
@@ -605,6 +631,7 @@ export async function executeQueries(
 
   for (const query of queries) {
     try {
+      hasTurso();
       //console.log(query);
       const result = await turso.execute(query);
       results.push(result);
